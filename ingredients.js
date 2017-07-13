b0200f4bbdd1c30f50063fcf96f47943fc6f7611
Austin Thorne <acthorne00@gmail.com>
2017-07-13 21:26:24 +0000

    ingredients array

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var counter = 0;
+while (counter < ingredients.length) {
+  console.log(ingredients[counter]);
+  counter ++;
+}
+
+// Write a while loop that prints out the contents of ingredients:
+
+for (var repeat = 0; repeat < ingredients.length; repeat++) {
+  console.log(ingredients[repeat]);
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (var reverseArray = 7; reverseArray <= 8 && reverseArray >= 0; reverseArray--) {
+  console.log(ingredients[reverseArray]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
