f2673e50036eadc9d63011e049c0814489796409
Austin Thorne <acthorne00@gmail.com>
2017-07-14 01:38:50 +0000

    javascripting complete

---
+++ looplighthouse.js
@@ -0,0 +1,17 @@
+var number = 100;
+while (number <= 200) {
+  number++;
+  console.log(number)
+
+if (number % 3 == 0); {
+  console.log("Loopy");
+}
+
+else if (number % 4 == 0); {
+  console.log("Lighthouse")
+}
+
+else {
+  console.log(number)
+}
+}
